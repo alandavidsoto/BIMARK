3be9b77ffbc24c3300a7266bbb372972cfa4ea9b
alan soto <alansoto2015@outlook.com>
2021-12-17 00:25:59 -0300

    changed nosotros.js

--- src/componentes/nosotros/Informacion.js
+++ src/componentes/nosotros/Informacion.js
@@ -6,11 +6,10 @@ export const Informacion = () => {
         <Fragment>
             <section style={{width: "100%",maxWidth:"1300px",margin: "0px auto",padding: "5px",textAlign: "center",height: "75vh"}}>
                 <Titulo message="¿ Quienes somos ?" />
-                <p>Somos una empresa dedicada a la venta de productos Perecederos y electrodomesticos, Nacida en 2021  por
-                Alan david soto</p>
+                <p>Somos una empresa dedicada a la venta de productos alimenticios, creada en 2021 por Alan David soto</p>
                 <Titulo message="¿ Qué queremos ofrecerte ?" />
                 <p>
-                Nuestro principal objetivo sos vos. Nuestro cliente . las empresas no pueden sostenerse si no satisfacen las necesidades de sus consumidores.  Queremos Brindarte la mejor atencion y los mejores precios para que vuelvas a elegirnos una y otra vez, manteniendo una buena relacion entre vos y Bimark. 
+                Nuestro principal objetivo es el cliente. Las empresas no pueden sostenerse si no satisfacen las necesidades de sus consumidores.  Queremos Brindarte la mejor atención y los mejores precios para que vuelvas a elegirnos una y otra vez, manteniendo una buena relación entre vos y Bimark. 
                 </p>
             </section>
             

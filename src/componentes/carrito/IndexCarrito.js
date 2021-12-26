4cc9efbd3cc219a797132110c97c3bdde4d61097
alan soto <alansoto2015@outlook.com>
2021-12-25 21:14:07 -0300

    changed navbar and indexCarrito

--- src/componentes/carrito/IndexCarrito.js
+++ src/componentes/carrito/IndexCarrito.js
@@ -1,48 +1,61 @@
-import './IndexCarrito.scss'
-import React, { Fragment } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { BanderaCategoria } from '../productos/BanderaCategoria'
-import { pay_product_action, remove_product_action} from '../../redux/actions/action'
-import { ButtonAgregar } from '../buttonAgregar/ButtonAgregar'
-import { SweetAlert } from '../../util/SetupAlert'
-import { ItemCarrito } from './ItemCarrito'
+import "./IndexCarrito.scss";
+import React, { Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { BanderaCategoria } from "../productos/BanderaCategoria";
+import {
+  pay_product_action,
+  remove_product_action,
+} from "../../redux/actions/action";
+import { ButtonAgregar } from "../buttonAgregar/ButtonAgregar";
+import { SweetAlert } from "../../util/SetupAlert";
+import { ItemCarrito } from "./ItemCarrito";
 
 const IndexCarrito = () => {
-    const productos = useSelector((state)=> state.reducer_product.product)
-    const dispatch = useDispatch()
-    
-    const acumuladorPrecios = () =>{
-        let result = 0;
-        productos.map(prod => result += (prod.precioFinal * prod.cantidad))
-        return result
-    }
-    const gratitude = ()=>{
-        SweetAlert("success","Muchas gracias por su compra")
-        dispatch(pay_product_action())
+  const productos = useSelector((state) => state.reducer_product.product);
+  const dispatch = useDispatch();
 
-    }
-    return (
-        <Fragment>
-            <BanderaCategoria categoria="Carrito"/>
-            <section className="carrito-productos" style={{minHeight: "70vh",}}>
-                {(productos.length > 0) && productos.map(el => <ItemCarrito key={el.id}  elemento={el}/>)}
-                {(productos.length > 0)? 
-                <>
-                    <article className="item-total" >
-                        <div style={{width: "80%"}}>TOTAL</div>
-                        <div style={{width: "20%"}}>$ {acumuladorPrecios()}</div>
-                    </article>
-                    <article className="item-total" style={{marginTop: "10px"}}>
-                        <div style={{width: "80%"}}></div>
-                        <div style={{width: "20%"}}><ButtonAgregar onClick={gratitude} style={{padding: "10px",width: "90%"}} >Pagar</ButtonAgregar></div>
-                    </article>
-                </>
-                 :
-                <div style={{textAlign: "center"}}>No hay items en el carrito. Agrega productos para visualizarlos en esta seccion.</div> 
-                }
-            </section>
-        </Fragment>
-    )
-}
+  const acumuladorPrecios = () => {
+    let result = 0;
+    productos.map((prod) => (result += prod.precioFinal * prod.cantidad));
+    return result;
+  };
+  const gratitude = () => {
+    SweetAlert("success", "Muchas gracias por su compra");
+    dispatch(pay_product_action());
+  };
+  return (
+    <Fragment>
+      <BanderaCategoria categoria="Carrito" />
+      <section className="carrito-productos" style={{ minHeight: "70vh" }}>
+        {productos.length > 0 &&
+          productos.map((el) => <ItemCarrito key={el.id} elemento={el} />)}
+        {productos.length > 0 ? (
+          <>
+            <article className="item-total">
+              <div style={{ width: "80%" }}>TOTAL</div>
+              <div style={{ width: "20%" }}>$ {acumuladorPrecios()}</div>
+            </article>
+            <article className="item-total" style={{ marginTop: "10px" }}>
+              <div style={{ width: "80%" }}></div>
+              <div style={{ width: "20%" }}>
+                <ButtonAgregar
+                  onClick={gratitude}
+                  style={{ padding: "10px", width: "90%", fontSize: "1em" }}
+                >
+                  Pagar
+                </ButtonAgregar>
+              </div>
+            </article>
+          </>
+        ) : (
+          <div style={{ textAlign: "center" }}>
+            No hay items en el carrito. Agrega productos para visualizarlos en
+            esta seccion.
+          </div>
+        )}
+      </section>
+    </Fragment>
+  );
+};
 
 export default IndexCarrito;
--- src/componentes/header/Header.js
+++ src/componentes/header/Header.js
@@ -1,67 +1,74 @@
-import './Header.scss'
-import React, {useState } from 'react'
-import menu from '../../images/menu.png'
-import { Buscador } from './Buscador';
-import { NavBar } from './NavBar';
-import { Link } from 'react-router-dom';
-import { Carrito } from './Carrito';
-import { useMediaQuery } from '../../hooks/useMediaQuery';
+import "./Header.scss";
+import React, { useState } from "react";
+import menu from "../../images/menu.png";
+import { Buscador } from "./Buscador";
+import { NavBar } from "./NavBar";
+import { Link } from "react-router-dom";
+import { Carrito } from "./Carrito";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 
 export const HeaderMobile = () => {
-    const [modalNav,setModalNav] = useState(false)
-    function modalMenu(){
-        const navBar = document.querySelector(".row-navbar");
-        navBar.style.left = "0%"
-        setModalNav(true)
-        navBar.addEventListener("click",(e)=> {
-            if(e.target.className == 'row-navbar' || e.target.tagName == "A"){
-                navBar.style.left = "-100%"
-            }
-        })
-    }
-    return (
-    <header  className="cabecera_mobile" >
-        <NavBar />
-        <div className="row-header">
-            <div onClick={modalMenu} className="hamburger">
-                <img src={menu} alt="menu"/>
-            </div>
-            <Link className="h1" to="/"><span>B</span>IMARK</Link>
-            {/* <Usuario /> */}
-            <Carrito />
+  const [modalNav, setModalNav] = useState(false);
+  function modalMenu() {
+    const navBar = document.querySelector(".row-navbar");
+    navBar.style.left = "0%";
+    setModalNav(true);
+    document.body.style.overflow = "hidden";
+    navBar.addEventListener("click", (e) => {
+      if (e.target.className == "row-navbar" || e.target.tagName == "A") {
+        navBar.style.left = "-100%";
+        document.body.style.overflow = "scroll";
+      }
+    });
+  }
+  return (
+    <header className="cabecera_mobile">
+      <NavBar />
+      <div className="row-header">
+        <div onClick={modalMenu} className="hamburger">
+          <img src={menu} alt="menu" />
         </div>
-        <Buscador  />
+        <Link className="h1" to="/">
+          <span>B</span>IMARK
+        </Link>
+        {/* <Usuario /> */}
+        <Carrito />
+      </div>
+      <Buscador />
     </header>
-    )
-}
+  );
+};
 export const Header_Desktop = () => {
-    return (
+  return (
     <header className="cabecera_desktop">
-        <div className="row-navbar">
-            <nav className="navbar">
-                <ul >
-                    <li><Link to="/productos">Productos</Link></li>
-                    <li><Link to="/ofertas">Ofertas</Link></li>
-                    <li><Link to="/nosotros">Nosotros</Link></li>
-                </ul>
-            </nav>
-        </div>
-        <div className="row-header">
-            <Link className="h1" to="/"><span>B</span>IMARK</Link>
-            <Buscador />
-            <Carrito />
-        </div>
+      <div className="row-navbar">
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to="/productos">Productos</Link>
+            </li>
+            <li>
+              <Link to="/ofertas">Ofertas</Link>
+            </li>
+            <li>
+              <Link to="/nosotros">Nosotros</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="row-header">
+        <Link className="h1" to="/">
+          <span>B</span>IMARK
+        </Link>
+        <Buscador />
+        <Carrito />
+      </div>
     </header>
-    )
-}
+  );
+};
 
 export const Header = () => {
-    const media_desktop = useMediaQuery(950,2000)
-    
-    return (
-        <>
-        {(media_desktop)? <Header_Desktop /> : <HeaderMobile />}
-        </>
-    )
-}
+  const media_desktop = useMediaQuery(950, 2000);
 
+  return <>{media_desktop ? <Header_Desktop /> : <HeaderMobile />}</>;
+};
